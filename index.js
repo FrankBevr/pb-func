98c51c75dc277d9449b5e6dbb8dd8deca6843be3
Frank Dierolf <frank_dierolf@web.de>
2024-07-24 10:27:47 +0200

    :zap: (.) adjust

--- index.js
+++ index.js
@@ -7,10 +7,14 @@ global.EventSource = eventsource;
 const openai = new OpenAI({ apiKey: "sk-None-oBBwp2KXurrQSZEaqDGST3BlbkFJ0o9LvP9UMKoPe3RGn5W3" });
 
 async function main() {
-  const image = await openai.images.generate({ prompt: "A whimsy cartoon frog sticker illustration isolated on a solid black background. The fro in the sticker is depicted in a watercolor style with dark and vibrant colors. Add gothic magic elements to the frog such as dark flowers, black feathers, amethyst crystals, and withered leaves. Ensure that the frog is centered and fits within the sticker's borders. The sticker should have a white border and a clipart-like appearance, embodying a mystical and enchanting vibe." })
+  const image = await openai.images.generate({
+    prompt: `
+
+
+` })
   console.log(image.data[0].url);
-  const rbgResultData = await removeBg(image.data[0].url);
-  fs.writeFileSync("no-bg.png", Buffer.from(rbgResultData));
+  // const rbgResultData = await removeBg(image.data[0].url);
+  // fs.writeFileSync("no-bg.png", Buffer.from(rbgResultData));
 }
 
 async function removeBg(imageURL) {
@@ -31,21 +35,4 @@ async function removeBg(imageURL) {
   }
 }
 
-const pb = new PocketBase('https://pocketbase.frank.gdn');
-pb.collection("items").subscribe('*', async (e) => {
-  try {
-    if (e.action === "create") {
-      await main()
-      const formData = new FormData()
-      formData.append("image", new Blob([fs.readFileSync("./no-bg.png")]), "froggy.png");
-      const record = await pb.collection('items').update(e.record.id, formData);
-      console.log(record)
-    } else {
-      console.log("Its updated")
-      fs.unlink("./no-bg.png", () => console.log("Its cleaned up"))
-    }
-  } catch (e) {
-    console.log(e)
-  }
-})
-
+main()
